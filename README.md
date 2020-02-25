f163276428ce1b15eaef03c3e0839031f0ca1753
Collin <collinw99@gmail.com>
2020-02-24 21:06:14 -0700

    Updating README and adding comments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+# Setup after cloning repository
+install dependencies with
+`npm install`
+start API with
+`npm run start`
+
 # API
-Central API waiting for requests
+Central API waiting for POST requests. Requests will be compared to valid values and a JSON object will be returned indicating if it is a valid value or not.
--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express()
 const bodyparser = require("body-parser")
 const fs = require("fs") // file system
 
+// port for API to listen on
 const port = process.env.KeyPadLockPort || 3000
 
+// console colors
 const cyan = "\x1b[36m"
 const red = "\x1b[31m"
 const reset = "\x1b[0m"
@@ -17,20 +19,26 @@ app.listen(port, () => {
     console.log(`API listening on port ${port}`)
 })
 
+// handling incoming post request
 app.post("/auth", (req, res) => {
+    // get the provided password
     const givenKey = req.body.pass
     let isValid = false
 
+    // read in the valid values from valid.txt file
     let validKeys = fs.readFileSync("valid.txt", "utf8")
     validKeys = validKeys.split('\n')
 
+    // loop through valid values and compare them to the given key
     for(i in validKeys) {
         if(validKeys[i] === givenKey) {
+            // valid key identified
             isValid = true
             break
         }
     }
 
+    // return proper response
     if(isValid) {
         console.log(cyan, `Received valid key ${givenKey}`, reset)
         res.status(200).json({isValid: true})
